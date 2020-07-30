0361f670fb81abbba7860472ae4e719d8887c9da
Ashok Kumar D <goldenashok@users.noreply.github.com>
2020-07-30 19:00:25 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home/home.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
